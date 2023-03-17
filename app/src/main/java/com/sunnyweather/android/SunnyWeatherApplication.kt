6418808f0bbe09dc5d8c0d33be5ccb52f8e5fdcf
Lily-508 <1135299049@qq.com>
2023-03-17 17:40:35 +0800

    刷新数据和切换城市天气实现

--- app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
+++ app/src/main/java/com/sunnyweather/android/SunnyWeatherApplication.kt
@@ -3,7 +3,7 @@ package com.sunnyweather.android
 import android.annotation.SuppressLint
 import android.app.Application
 import android.content.Context
-
+//在AndroidManifest的application设置就能全局获取Context对象
 class SunnyWeatherApplication:Application() {
     companion object{
         @SuppressLint("StaticFieldLeak")
