5e19cb26968f30b60becf93a8aa9fe05764fc10b
Lily-508 <1135299049@qq.com>
2023-03-17 15:42:12 +0800

    使用SharedPreference存储数据

--- app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
+++ app/src/main/java/com/sunnyweather/android/ui/place/PlaceViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.sunnyweather.android.logic.Repository
+import com.sunnyweather.android.logic.dao.PlaceDao
 import com.sunnyweather.android.logic.model.Place
 
 class PlaceViewModel:ViewModel(){
@@ -15,4 +16,7 @@ class PlaceViewModel:ViewModel(){
     fun searchPlaces(query:String){
         searchLiveData.value=query
     }
+    fun savePlace(place: Place)= PlaceDao.savePlace(place)
+    fun getSavedPlace()= PlaceDao.getSavedPlace()
+    fun isPlaceSaved()= PlaceDao.isPlaceSaved()
 }
